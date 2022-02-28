c5bd38ba704f21f9fd59d1d6fce7060efa8920d7
Eric Rackauckas <erackauckas@gmail.com>
2022-02-27 21:35:03 -0800

    commit tree 25204bb2ef82e19977f363d9f5ad377e73bd6a05

---
+++ index.js
@@ -0,0 +1,57 @@
+function add() {
+    // we'll fill this in shortly
+  }
+
+  function subtract() {
+    // we'll fill this in shortly
+  }
+
+  function multiply() {
+    // we'll fill this in shortly
+  }
+
+  function divide() {
+    // we'll fill this in shortly
+  }
+
+  function add(a, b) {
+      return a+b;
+    // we'll fill this in shortly
+  }
+
+  function subtract(a, b) {
+    return a-b;
+  // we'll fill this in shortly
+}
+
+function multiply(a, b) {
+    return a*b;
+  // we'll fill this in shortly
+}
+
+function divide(a, b) {
+    return a/b;
+  // we'll fill this in shortly
+}
+
+let n= 10
+
+function increment(n) {
+    return n+=1;
+  // we'll fill this in shortly
+}
+
+function decrement(n) {
+    return n-=1;
+  // we'll fill this in shortly
+}
+
+function makeInt(n) {
+    return parseInt(n, 10)
+  // we'll fill this in shortly
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n)
+  // we'll fill this in shortly
+}
